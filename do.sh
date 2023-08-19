d010db175b3e599fe6f545dd3146c70acbeaaa41
Karris <karriszou@outlook.com>
2023-08-19 14:57:49 +0800

    Update do.sh

--- do.sh
+++ do.sh
@@ -40,9 +40,10 @@ sudo sysctl net.ipv4.tcp_congestion_control
 cat ssh-key-001.pub >> ~/.ssh/authorized_keys
 
 # Enable PubkeyAuthentication
-echo PubkeyAuthentication yes >> /etc/ssh/sshd_config
 echo PasswordAuthentication no >> /etc/ssh/sshd_config
-sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config && service ssh restart
+echo PubkeyAuthentication yes >> /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sudo service ssh restart
 # sudo systemctl restart sshd.service
 # sudo systemctl restart ssh.service
 
